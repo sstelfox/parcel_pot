991c925c375b737eb805497d16a348a4215b03af
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-20 12:23:49 -0400

    Changed a few cap settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,14 @@
-set :application, 'my app name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+
+set :application, 'parcel_pot'
+set :repo_url,    'git@github.com:sstelfox/parcel_pot.git'
+
+# This should eventually be set by the environment
+set :branch, 'master'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :deploy_to, "/var/www/#{fetch(:application)}"
+set :scm, :git
 
 # set :format, :pretty
 # set :log_level, :debug
@@ -17,7 +21,6 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :keep_releases, 5
 
 namespace :deploy do
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -36,5 +39,5 @@ namespace :deploy do
   end
 
   after :finishing, 'deploy:cleanup'
-
 end
+
