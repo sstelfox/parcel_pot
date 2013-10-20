a0f7b86cd0fb77be17efa6dca8a0f24d53774533
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2013-10-19 22:36:42 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+
+#gem 'sass-rails', '~> 4.0.0'
+#gem 'uglifier', '>= 1.3.0'
+#gem 'coffee-rails', '~> 4.0.0'
+#gem 'therubyracer', platforms: :ruby
+
+gem 'jquery-rails'
+gem 'turbolinks'
+#gem 'jbuilder', '~> 1.2'
+
+# Use ActiveModel has_secure_password
+#gem 'bcrypt-ruby', '~> 3.0.0'
+
+gem 'unicorn'
+
+group :development do
+  gem 'capistrano'
+  gem 'sqlite3'
+end
+
+group :development, :test do
+  gem 'debugger'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParcelPot::Application.config.secret_key_base = '2d39ac8a6ca000cedd99e30ffd525997046f201b74b0971e03831683065f75c90455c9430b8e569b2eb0998f3950bad4733c70b14eb559144988d4aba2173bca'
